b27dad71bdd6bb796ef15d4e78507e02c6d9d28c
trive <mtriverio@Dieter.local>
2015-03-05 18:15:58 -0800

    Uninstaller version 1.0

--- Uninstaller/script.sh
+++ Uninstaller/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Start
+echo "Starting uninstaller..."
+PROGRESS:1
 
-# Get rid of old versions in user folder
+# Get rid of files in user folder
+echo "Removing Origami and Avocado from user's library..."
+PROGRESS:10
 rm -rf ~/Library/Graphics/Quartz\ Composer\ Plug-Ins/Keyboard\ 2.plugin
 rm -rf ~/Library/Graphics/Quartz\ Composer\ Plug-Ins/RetrieveGlobalValue.plugin
 rm -rf ~/Library/Graphics/Quartz\ Composer\ Plug-Ins/SaveGlobalVariable.plugin
@@ -11,7 +16,9 @@ rm -rf ~/Library/Graphics/Quartz\ Composer\ Patches/Avocado
 rm -rf ~/Library/Graphics/Quartz\ Composer\ Patches/Origami
 rm -rf ~/Library/Graphics/Quartz\ Composer\ Patches/Origami.plugin
 
-# Get rid of old versions in system folder
+# Get rid of files in system folder
+echo "Removing Origami and Avocado from system's library..."
+PROGRESS:30
 rm -rf /Library/Graphics/Quartz\ Composer\ Plug-Ins/Keyboard\ 2.plugin
 rm -rf /Library/Graphics/Quartz\ Composer\ Plug-Ins/RetrieveGlobalValue.plugin
 rm -rf /Library/Graphics/Quartz\ Composer\ Plug-Ins/SaveGlobalVariable.plugin
@@ -22,12 +29,15 @@ rm -rf /Library/Graphics/Quartz\ Composer\ Patches/Origami
 rm -rf /Library/Graphics/Quartz\ Composer\ Patches/Origami.plugin
 
 # Get rid of temporary files
+echo "Performing additional cleanup..."
+PROGRESS:50
 rm -rf /Users/Shared/Origami
 rm -rf /Users/Shared/Origami.plugin
 rm -rf /Users/Shared/Avocado
 rm -rf /Users/Shared/Avocado\ Plug-Ins/
 
 # Set permissions correctly
+PROGRESS:70
 chown $USER:`id -g $USER` ~/Library/Graphics
 chown $USER:`id -g $USER` ~/Library/Graphics/*
 chown -R $USER:`id -g $USER` ~/Library/Graphics/Quartz\ Composer\ Patches
@@ -37,3 +47,9 @@ chown -R $USER:`id -g $USER` ~/Library/Graphics/Quartz\ Composer\ Plug-Ins
 chown $USER:`id -g $USER` ~/Library/Graphics/Quartz\ Composer\ Plug-Ins
 chown $USER:`id -g $USER` ~/Library/Graphics/Quartz\ Composer\ Plug-Ins/*
 
+# End
+echo "Origami and Avocado have been successfully uninstalled."
+echo ""
+echo "To reinstall Origami please visit: http://origami.facebook.com"
+echo "To reinstall both Avocado and Origami please visit: http://avocado.ideo.com"
+PROGRESS:100
